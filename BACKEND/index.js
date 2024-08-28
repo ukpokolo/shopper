8b915266763f431314ce980ed1ce68fb05b89f9a
ukpokolo <jiukpokolo@gmail.com>
2024-08-28 14:49:39 +0100

    Update index.js

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -17,11 +17,7 @@ const allowCors = fn => async (req, res) => {
     'Access-Control-Allow-Headers',
     'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
   );
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
+  
 };
 
 // Database Connection with MongoDB
